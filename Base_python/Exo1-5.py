8099bc0f86e7a5b13dd9700c27bc6bf769846691
Namelessking1308 <ibrahimfakir485@gmail.com>
2026-05-05 11:51:40 +0200

    correctifs console

---
+++ Base_python/Exo1-5.py
@@ -0,0 +1,24 @@
+nom = input("Entrez votre nom: ")
+prenom = input("Entrez votre prénom: ")
+
+print("Bonjour:", nom, prenom, sep=" ", end="!\n")
+
+print("Message 1\nMessage 2\nMessage 3")
+
+mot1 = input("Entrez un mot: ")
+mot2 = input("Entrez un mot: ")
+
+print("Votre mots: ", mot1, mot2, sep="", end="!\n")
+
+nb1 = int(input("Entrez un nombre: "))
+nb2 = int(input("Entrez un nombre: "))
+
+print("Somme", nb1 + nb2, sep=" = ", end="!\n")
+
+val1 = int(input("Entrez une valeur: "))
+val2 = int(input("Entrez une valeur: "))
+val3 = int(input("Entrez une valeur: "))
+
+print("Vos valeur", val1, val2, val3, sep=" -> ", end=" Wow!")
+
+
